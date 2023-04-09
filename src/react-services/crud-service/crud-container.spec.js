aea0f01214c821196a1f05d36ca7395f90833af2
itechdom <oalghnmi@gmail.com>
2023-04-09 23:53:33 +0300

    tests

--- src/react-services/crud-service/crud-container.spec.js
+++ src/react-services/crud-service/crud-container.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { CrudContainerFP } from '../crud-container';
+import { CrudContainerFP } from './crud-container';
 
 describe('CrudContainerFP', () => {
   const props = {
