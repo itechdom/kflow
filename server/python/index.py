b4d6bca6c889551e7250d37b3826200bcfda8e53
itechdom <oalghnmi@gmail.com>
2023-04-12 01:27:22 +0300

    clean up python folder'

--- server/python/index.py
+++ server/python/index.py
@@ -1,9 +1,2 @@
-# from src import app
-# app.run()
-from src import getMostUniqueWords
-import json
-
-mat = getMostUniqueWords()
-f = open("wiki-res.json", "w")
-f.write(json.dumps(mat))
-f.close()
+from src import app
+app.run()
--- server/python/src/__init__.py
+++
@@ -1,2 +0,0 @@
-from .server import app
-from .mostUniqueWords import getMostUniqueWords
