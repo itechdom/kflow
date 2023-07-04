c777380824ba5a5ff306de699fbe40c52010e8a6
itechdom <oalghnmi@gmail.com>
2023-07-04 20:57:58 +0300

    torch workflow

--- server/python/src/torch-workflow.py
+++ server/python/src/torch-workflow.py
@@ -1,13 +1,14 @@
+#@title Default title text
 from matplotlib import pyplot as plt
 import torch
-from torch import nn 
+from torch import nn
 
 #nn consists of layers
 
 # our goal is to  get data into a numerical representation that we can feed into a neural network
-# we can find patterns 
+# we can find patterns
 
-# linear regression formula to make a straight line 
+# linear regression formula to make a straight line
 weight = 0.7
 bias = 0.3
 
@@ -53,3 +54,74 @@ def plot_predictions(train_data=X_train,
 
 #plot the training data and test data
 plot_predictions()
+
+#build a model
+class LinearRegressionModel(nn.Module):
+  def __init__(self):
+    super().__init__()
+    # parameters we need to learn
+    # we start with random and then eventually reach the above graph
+    self.weights = nn.Parameter(torch.randn(1))
+    self.bias = nn.Parameter(torch.randn(1))
+    #torch.optim: contains algorithms needed to figure out the parameters
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    return self.weights * x + self.bias
+
+#Create random seed
+torch.manual_seed(42)
+
+#crete an instance of the model
+model_0 = LinearRegressionModel()
+
+# we want our parameters
+list(model_0.parameters())
+#to be close to bias, weight
+bias, weight
+
+### making prediction using torch.inference_mode()
+### it removes gradient decent tracking
+### to check our model's prediction, let's see how it predicts y_test based on X_test
+# with torch.inference_mode():
+#   y_preds = model_0(X_test)
+# y_preds
+
+#plot_predictions(predictions=y_preds)
+
+### to correct these predictions we need to use a loss function (cost function) measure how wrong the model is
+loss_fn = nn.L1Loss()
+### optimizer takes into account the loss and adjust the model's parameters
+optimizer = torch.optim.SGD(params=model_0.parameters(),
+                            lr=0.1) #hyper parameter is the most important one
+### let's setup training loop
+### 0. Loop through the data
+### 1. Forward pass (forward propagation)
+### 2. Calculate the loss
+### 3. Optimizer zero grad
+### 4. Loss backward - move backwards to calcuate the gradients
+### 5. Optimzier step - use the optimizer to adjust the parameters
+epochs = 1000 # one loop through the data
+for epoch in range(epochs):
+  # Set the model to training mode
+  model_0.train() # set gradients in each run
+  #1. Forward pass
+  y_pred = model_0(X_train)
+  #2. Caclulate loss
+  loss = loss_fn(y_pred, y_train)
+  #print(f"Loss: {loss}")
+  #3. optimizer zero grad (refresh the gradient descent value since it accumulates for each loop)
+  optimizer.zero_grad()
+  #4. performs backpropagation on the loss with respect to the parameters of the model
+  loss.backward()
+  #5. Step the optimizer (perform gradient descent)
+  optimizer.step()
+  ### testing loop
+  model_0.eval()
+  with torch.inference_mode():
+    #forward pass
+    test_pred = model_0(X_test)
+    #2. Calculate the loss
+    test_loss = loss_fn(test_pred, y_test)
+  if epoch % 10 == 0:
+    print(f"Epoch {epoch} | Loss: {test_loss}")
+    print(model_0.state_dict())
+plot_predictions(predictions=test_pred)
