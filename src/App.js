3a4c69959d409a4a9d160c41a713793a77f08f59
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-09 02:51:43 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { Switch, HashRouter as Router } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
 import Profile from "./Profile/Profile";
 import Admin from "./Admin/Admin";
 import theme from "./theme";
 import { styles } from "./App.styles.js";
-import { withStyles } from "@material-ui/core/styles";
 import "./global.css";
 import KnowledgeList from "./Pages/KnowledgeList";
 import KnowledgeDetails from "./Pages/KnowledgeDetails";
+
 class App extends React.Component {
   render() {
     const { classes } = this.props;
     return (
       <Router>
         <Switch>
-          <Profile classes={classes} />
-          <Admin classes={classes} />
-          <KnowledgeDetails classes={classes} />
-          <KnowledgeList classes={classes} />
+          <Route path="/profile">
+            <Profile classes={classes} />
+          </Route>
+          <Route path="/profile">
+            <Admin classes={classes} />
+          </Route>
+          <Route path="/knowledge">
+            <KnowledgeDetails classes={classes} />
+          </Route>
+          <Route path="/">
+            <KnowledgeList classes={classes} />
+          </Route>
         </Switch>
       </Router>
     );
