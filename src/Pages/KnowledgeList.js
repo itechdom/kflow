f6dc0fc629b497f02c4aaa6f1611fa6a9679bbe1
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-09 01:43:35 -0400

    knowledge list

--- src/Pages/KnowledgeList.js
+++ src/Pages/KnowledgeList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import { Crud } from "Libs/react-services/crud-service/crud-container";
 import MainWrapper from "Libs/orbital-templates/Material/Wrappers/MainWrapper";
 import Loading from "Libs/orbital-templates/Material/_shared/Loading/Loading";
@@ -11,97 +10,90 @@ import { offlineStorage } from "../OfflineStorage";
 import rootStore from "../Store/reduxStore";
 const logo = "images/logo-no-background.svg";
 
-const KnowledgeList = ({ classes }) => {
+const KnowledgeList = ({ classes, routeProps }) => {
+  const {
+    match: { params },
+  } = routeProps;
+  let query = { _id: params.id };
   return (
-    <Route
-      path={`${this.props.match.path}`}
-      render={(routeProps) => {
-        const {
-          match: { params },
-        } = routeProps;
-        let query = { _id: params.id };
+    <Crud
+      modelName="knowledge"
+      SERVER={config.SERVER}
+      offlineStorage={offlineStorage}
+      notificationDomainStore={rootStore.notificationDomainStore}
+      crudDomainStore={rootStore.crudDomainStore}
+      paginate={false}
+      query={query}
+      render={(props) => {
+        let knowledge = props.knowledge;
+        console.log("KNowldge search model", props);
+        if (!knowledge || props.knowledge_loading) {
+          return <Loading></Loading>;
+        }
         return (
-          <Crud
-            modelName="knowledge"
-            SERVER={config.SERVER}
-            offlineStorage={offlineStorage}
-            notificationDomainStore={rootStore.notificationDomainStore}
-            crudDomainStore={rootStore.crudDomainStore}
-            paginate={false}
-            query={query}
-            render={(props) => {
-              let knowledge = props.knowledge;
-              console.log("KNowldge search model", props);
-              if (!knowledge || props.knowledge_loading) {
-                return <Loading></Loading>;
+          <MainWrapper
+            logo={logo}
+            routeList={[
+              {
+                url: "all",
+                name: "All",
+                icon: "",
+              },
+            ]}
+            drawerRouteList={
+              this.state.currentUser && this.state.currentUser.isAdmin
+                ? [...mainRouteList, logoutRoute]
+                : [...mainRouteList, logoutRoute]
+            }
+            user={this.state.currentUser}
+            {...routeProps}
+            {...this.props}
+            onRouteClick={(route) => {
+              this.setState({
+                tags: new Set([]),
+              });
+              if (route.indexOf("http") !== -1) {
+                return window.open(route);
               }
-              return (
-                <MainWrapper
-                  logo={logo}
-                  routeList={[
-                    {
-                      url: "all",
-                      name: "All",
-                      icon: "",
-                    },
-                  ]}
-                  drawerRouteList={
-                    this.state.currentUser && this.state.currentUser.isAdmin
-                      ? [...mainRouteList, logoutRoute]
-                      : [...mainRouteList, logoutRoute]
-                  }
-                  user={this.state.currentUser}
-                  {...routeProps}
-                  {...this.props}
-                  onRouteClick={(route) => {
-                    this.setState({
-                      tags: new Set([]),
-                    });
-                    if (route.indexOf("http") !== -1) {
-                      return window.open(route);
-                    }
-                    return routeProps.history.push(`${route}`);
-                  }}
-                  classes={{
-                    ...classes,
-                    tabMenu: `${classes["white"]}`,
-                    menuTabsClasses: {
-                      flexContainer: `${classes["center"]}`,
-                    },
-                  }}
-                >
-                  <Wikipedia
-                    SERVER={config.SERVER}
-                    offlineStorage={offlineStorage}
-                    notificationDomainStore={rootStore.notificationDomainStore}
-                  >
-                    <Knowledge
-                      onEdit={(model) => {
-                        routeProps.history.push(`//edit/${model._id}`);
-                      }}
-                      onDelete={() => {
-                        routeProps.history.goBack();
-                      }}
-                      match={routeProps.match}
-                      history={routeProps.history}
-                      classes={classes}
-                      location={this.props.location}
-                      currentTags={this.state.tags}
-                      selected={this.state.selected}
-                      currentUser={this.state.currentUser}
-                      setState={(props) => this.setState(props)}
-                      model={knowledge}
-                      loading={props.knowledge_loading}
-                      knowledge={knowledge}
-                      knowledge_updateModel={props.knowledge_updateModel}
-                      knowledge_deleteModel={props.knowledge_deleteModel}
-                      knowledge_searchModel={props.knowledge_searchModel}
-                    />
-                  </Wikipedia>
-                </MainWrapper>
-              );
+              return routeProps.history.push(`${route}`);
             }}
-          />
+            classes={{
+              ...classes,
+              tabMenu: `${classes["white"]}`,
+              menuTabsClasses: {
+                flexContainer: `${classes["center"]}`,
+              },
+            }}
+          >
+            <Wikipedia
+              SERVER={config.SERVER}
+              offlineStorage={offlineStorage}
+              notificationDomainStore={rootStore.notificationDomainStore}
+            >
+              <Knowledge
+                onEdit={(model) => {
+                  routeProps.history.push(`//edit/${model._id}`);
+                }}
+                onDelete={() => {
+                  routeProps.history.goBack();
+                }}
+                match={routeProps.match}
+                history={routeProps.history}
+                classes={classes}
+                location={this.props.location}
+                currentTags={this.state.tags}
+                selected={this.state.selected}
+                currentUser={this.state.currentUser}
+                setState={(props) => this.setState(props)}
+                model={knowledge}
+                loading={props.knowledge_loading}
+                knowledge={knowledge}
+                knowledge_updateModel={props.knowledge_updateModel}
+                knowledge_deleteModel={props.knowledge_deleteModel}
+                knowledge_searchModel={props.knowledge_searchModel}
+              />
+            </Wikipedia>
+          </MainWrapper>
         );
       }}
     />
--- src/Pages/Me.js
+++ src/Pages/Me.js
@@ -1,51 +1,32 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import { MainWrapper } from "../Components";
 import { mainRouteList, logoutRoute } from "../../Routes";
-import {
-  Crud,
-  Forms,
-  Media,
-  Notification,
-  Profile as ProfileComponent,
-} from "Libs/orbital-templates/Material";
-import { config } from "../../config";
-import { offlineStorage } from "../../offlineStorage";
-import { rootStore } from "../../Store/reduxStore";
 
-const Me = ({ classes }) => {
+const Me = ({ classes, routeProps }) => {
   return (
-    <Route
-      path={`${this.props.match.path}me`}
-      render={(routeProps) => {
-        return (
-          <MainWrapper
-            routeList={mainRouteList}
-            onDrawerRouteClick={(route) => {
-              if (route.indexOf("http") !== -1) {
-                return window.open(route);
-              }
-              return routeProps.history.push(`${route}`);
-            }}
-            drawerRouteList={
-              this.state.currentUser && this.state.currentUser.isAdmin
-                ? [...mainRouteList, logoutRoute]
-                : [...mainRouteList, logoutRoute]
-            }
-            {...routeProps}
-            {...this.props}
-            isTabMenu={true}
-            classes={classes}
-            onRouteClick={(route) => {
-              if (route.indexOf("http") !== -1) {
-                return window.open(route);
-              }
-              return routeProps.history.push(`${route}`);
-            }}
-          >
-          </MainWrapper>
-        );
+    <MainWrapper
+      routeList={mainRouteList}
+      onDrawerRouteClick={(route) => {
+        if (route.indexOf("http") !== -1) {
+          return window.open(route);
+        }
+        return routeProps.history.push(`${route}`);
       }}
-    ></Route>
+      drawerRouteList={
+        this.state.currentUser && this.state.currentUser.isAdmin
+          ? [...mainRouteList, logoutRoute]
+          : [...mainRouteList, logoutRoute]
+      }
+      {...routeProps}
+      {...this.props}
+      isTabMenu={true}
+      classes={classes}
+      onRouteClick={(route) => {
+        if (route.indexOf("http") !== -1) {
+          return window.open(route);
+        }
+        return routeProps.history.push(`${route}`);
+      }}
+    ></MainWrapper>
   );
 };
