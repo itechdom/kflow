00fe01edf08c86232aaa1c9b310e0bebd34f9bc7
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-11 00:22:51 -0400

    custom styling

--- src/Knowledge/ModelPreview/CustomTreeNode.js
+++ src/Knowledge/ModelPreview/CustomTreeNode.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useRef } from "react";
 import SvgCircles from "./SvgCircles";
 
+function truncateText(text, maxLength) {
+    if (text.length > maxLength) {
+        return text.substring(0, maxLength - 3) + "...";
+    }
+    return text;
+}
+
 const CustomTreeNode = ({ nodeDatum, toggleNode }) => {
-  const MAX_TEXT_LENGTH= 20;
+  const MIN_TEXT_LENGTH = 20;
+  const MAX_TEXT_LENGTH= 95;
   const handleClick = () => {
     toggleNode(); // This function is provided by react-d3-tree to toggle the node
   };
@@ -10,6 +18,7 @@ const CustomTreeNode = ({ nodeDatum, toggleNode }) => {
   const [cy, setCY] = React.useState(30);
   const [radius, setRadius] = React.useState(100);
   const [pWidth, setPWidth] = React.useState(0);
+  const [pHeight, setPHeight] = React.useState(0);
   const pRef = useRef(null);
   const nodeStyles = {
     fill: "white",
@@ -34,12 +43,14 @@ const CustomTreeNode = ({ nodeDatum, toggleNode }) => {
   useEffect(() => {
     //truncate if length is greater than 30
     console.log("PWidth", pWidth);
+    console.log("PHeight", pHeight); 
     if (nodeDatum.name.length > MAX_TEXT_LENGTH) {
       setRadius(200);
-      setCx(60);
-      setCY(60);
-      const multiplier = nodeDatum.name.length / MAX_TEXT_LENGTH;
-      const pWidth = pRef.current.offsetWidth;
+      setCx(-300);
+      setCY(0);
+      if(nodeDatum.name.length > MAX_TEXT_LENGTH){
+        nodeDatum.name = truncateText(nodeDatum.name, MAX_TEXT_LENGTH);
+      }
     } else {
       setRadius(150);
       setCx(0);
@@ -49,11 +60,12 @@ const CustomTreeNode = ({ nodeDatum, toggleNode }) => {
     return () => {
       console.log("Node unmounted:", nodeDatum.name);
     };
-  }, [nodeDatum.name, pWidth, toggleNode]);
+  }, [nodeDatum, pWidth, pHeight, toggleNode]);
   // Measure the width of the <p> tag after component mounts
   useEffect(() => {
     if (pRef.current) {
       setPWidth(pRef.current.getBoundingClientRect().width);
+      setPHeight(pRef.current.getBoundingClientRect().height);
     }
   }, []);
   return (
