f6ad886340cb27ded71bebb0e6364f3254eab8b7
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-17 00:48:29 -0400

    use nx

--- src/Libs/react-services/utils/test-utils.js
+++
@@ -1,8 +0,0 @@
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-
-// Create a store for testing
-export const MockStore = ({ children, rootReducer }) => {
-  const store = createStore(rootReducer);
-  return <Provider store={store}>{children}</Provider>;
-};
