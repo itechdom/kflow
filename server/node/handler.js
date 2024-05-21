957452db2ef532c9cc64ae44aca08330475475c6
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-21 12:36:40 -0400

    get secret

--- server/node/handler.js
+++ server/node/handler.js
@@ -3,14 +3,12 @@ const serverless = require('serverless-http');
 const customConfig = require('./customConfig');
 const { getExpressApp, getAllApis, registerAllRoutes } = require('./server');
 
-const appConfig = customConfig; // Use customConfig here
-
 const initApp = async () => {
     // Initialize customConfig to fetch DB_URI
     await customConfig.initialize();
 
     // Initialize Express app
-    const { app, server } = getExpressApp(appConfig);
+    const { app, server } = getExpressApp(customConfig);
 
     const exceptions = { disableChat: false, disableRides: false };
     const apiRoutes = getAllApis({ app, server, exceptions });
