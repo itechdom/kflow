36d89b9f2b9bdd07a667d561fbf69f889a91384c
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-21 11:20:10 -0400

    handler

--- server/node/handler.js
+++ server/node/handler.js
@@ -1,6 +1,14 @@
-module.exports.run = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Lambda!' }),
-  };
-};
+'use strict';
+const serverless = require('serverless-http');
+const config = require('config');
+const { getExpressApp, getAllApis, registerAllRoutes } = require('./server');
+
+const appConfig = config; // Customize this if you need different configurations
+
+const { app, server } = getExpressApp(appConfig);
+
+const exceptions = { disableChat: false, disableRides: false };
+const apiRoutes = getAllApis({ app, server, exceptions });
+registerAllRoutes({ app, server, exceptions, ...apiRoutes });
+
+module.exports.handler = serverless(app);
