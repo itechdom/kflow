11a33cdb6b8899613c11f8217a062b106a53da7b
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-21 11:58:50 -0400

    handler

--- server/node/handler.js
+++ server/node/handler.js
@@ -1,14 +1,29 @@
 'use strict';
 const serverless = require('serverless-http');
 const config = require('config');
+const getAWSSecret = require('./getAWSSecret');
 const { getExpressApp, getAllApis, registerAllRoutes } = require('./server');
 
 const appConfig = config; // Customize this if you need different configurations
 
-const { app, server } = getExpressApp(appConfig);
+const initApp = async () => {
+    // Fetch the AWS Secret and connect to MongoDB
+    await getAWSSecret();
 
-const exceptions = { disableChat: false, disableRides: false };
-const apiRoutes = getAllApis({ app, server, exceptions });
-registerAllRoutes({ app, server, exceptions, ...apiRoutes });
+    // Initialize Express app
+    const { app, server } = getExpressApp(appConfig);
 
-module.exports.handler = serverless(app);
+    const exceptions = { disableChat: false, disableRides: false };
+    const apiRoutes = getAllApis({ app, server, exceptions });
+    registerAllRoutes({ app, server, exceptions, ...apiRoutes });
+
+    return app;
+};
+
+const handler = async (event, context) => {
+    const app = await initApp();
+    const serverlessHandler = serverless(app);
+    return serverlessHandler(event, context);
+};
+
+module.exports.handler = handler;
