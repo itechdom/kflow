71e1868ec235c3e701961e39ff15e047fd835da7
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-23 03:39:30 -0400

    version change

--- server/node/scripts/knowledge-base/utils.js
+++ server/node/scripts/knowledge-base/utils.js
@@ -1,42 +1,62 @@
 const MongoDb = require("@markab.io/orbital-api/MongoDb");
 const config = require("config");
-const fs = require("fs").promises;
+const fs = require("fs");
 const uuidv1 = require("uuid/v1");
 const path = require("path");
-
-module.exports.readFiles = async function readFiles(dirname, onFileContent, onError) {
-  try {
-    const filenames = await fs.readdir(path.join(__dirname, dirname));
-    for (const filename of filenames) {
-      try {
-        const content = await fs.readFile(path.join(__dirname, dirname, filename), "utf-8");
-        onFileContent(filename, content);
-      } catch (err) {
-        onError(err);
-      }
+module.exports.readFiles = function readFiles(dirname, onFileContent, onError) {
+  return fs.readdir(path.join(__dirname, dirname), function (err, filenames) {
+    if (err) {
+      onError(err);
+      return;
     }
-  } catch (err) {
-    onError(err);
-  }
+    filenames.forEach(function (filename) {
+      fs.readFile(path.join(__dirname, dirname) + filename, "utf-8", function (
+        err,
+        content
+      ) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
 };
-
+module.exports.connectToDb = async function connectToDb(cb) {
+  const dbConnection = await MongoDb({
+    config,
+    onDBInit: (data) => cb(null, data),
+    onError: (err) => cb("err", err),
+    onDisconnect: (err) => cb("disconnect", err),
+  });
+  return dbConnection;
+};
 module.exports.formatMindmap = function formatMindmap(node, path) {
   if (node) {
-    Object.keys(node).forEach((key, index) => {
+    Object.keys(node).map((key, index) => {
       let currentPath = path ? path + "." + `${index}` : "0";
       node[key].level = currentPath;
       node[key]._id = uuidv1();
-      formatMindmap(node[key].ideas, currentPath);
+      return formatMindmap(node[key].ideas, currentPath);
     });
   }
+  return;
 };
 
-module.exports.flattenMindmap = function flattenMindmap(node, parent, mindmapByKeys) {
+//TODO: flatten mindmap prior to saving and add links for graph viz
+module.exports.flattenMindmap = function flattenMindmap(
+  node,
+  parent,
+  mindmapByKeys
+) {
   if (node) {
     let group = parent && parseInt(parent.level.split(".").join(""));
     let size = 20 / (parent && parent.level.split(".").length);
-    Object.keys(node).forEach((key) => {
+    Object.keys(node).map((key, index) => {
       let currentNode = node[key];
+      // console.log(currentNode, "NODOODE");
+      // currentNode = this.formatMindmap
       const { title } = currentNode;
       mindmapByKeys[currentNode._id] = {
         title,
@@ -45,8 +65,15 @@ module.exports.flattenMindmap = function flattenMindmap(node, parent, mindmapByK
         size,
         group: group,
         level: currentNode.level,
-        attr: currentNode.attr && currentNode.attr.note && currentNode.attr.note.text,
-        children: currentNode.ideas ? Object.keys(currentNode.ideas).map((k) => currentNode.ideas[k]._id) : [],
+        attr:
+          currentNode.attr &&
+          currentNode.attr.note &&
+          currentNode.attr.note.text,
+        children: currentNode.ideas
+          ? Object.keys(currentNode.ideas).map((k) => {
+              return currentNode.ideas[k]._id;
+            })
+          : [],
         parent: parent && parent._id,
         links: {
           title: parent && parent.title,
@@ -58,4 +85,5 @@ module.exports.flattenMindmap = function flattenMindmap(node, parent, mindmapByK
       flattenMindmap(currentNode.ideas, currentNode, mindmapByKeys);
     });
   }
+  return;
 };
