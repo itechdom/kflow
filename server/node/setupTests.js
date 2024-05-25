29fdfb53305788d852c3a6985e4d52ea920050d4
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-24 21:55:51 -0400

    setup test

--- server/node/setupTests.js
+++ server/node/setupTests.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
+import {jest} from '@jest/globals';
 
 // Mock the database URI for testing purposes
 const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/testdb';
 
 beforeAll(async () => {
   // Connect to the database
-  await mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
 afterAll(async () => {
   // Disconnect from the database
-  await mongoose.disconnect();
 });
